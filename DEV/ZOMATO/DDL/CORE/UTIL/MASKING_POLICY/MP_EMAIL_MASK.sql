2f07d43c8746a0430ee3199a0233d536ead6c9ab
shobhith753 <shobhith753@gmail.com>
2025-11-15 21:18:44 +0530

    Delete DEV/ZOMATO directory

--- DEV/ZOMATO/DDL/CORE/UTIL/MASKING_POLICY/MP_EMAIL_MASK.sql
+++
@@ -1,7 +0,0 @@
-create or replace masking policy MP_EMAIL_MASK as (VAL VARCHAR) 
-returns VARCHAR ->
-CASE
-    WHEN CURRENT_ROLE() IN ('ZOMATO_PII_ADMIN_ROLE','ACCOUNTADMIN') THEN VAL
-    ELSE REGEXP_REPLACE(VAL, '(^.).*(@.*$)', '\\1*****\\2')
-  END
-;
